8cb8622df6db10199007c3ad1605bc2b8f392bd8
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-05-20 22:42:53 +0300

    0.1.1 release

---
+++ CHANGES.rst
@@ -0,0 +1,16 @@
+Changes
+=======
+
+.. contents::
+
+
+0.1.1 (2016-05-20)
+------------------
+
+* Do not use proxy when calling autologin http api.
+
+
+0.1.0 (2016-05-20)
+------------------
+
+Initial release
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='autologin-middleware',
-    version='0.1.0',
+    version='0.1.1',
     description='A Scrapy middleware to use with autologin',
     license='MIT',
     author='Konstantin Lopuhin, Mikhail Korobov',
