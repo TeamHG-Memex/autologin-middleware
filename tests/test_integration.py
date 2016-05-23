TeamHG-Memex/autologin-middleware

commit da9751bcc373087e2e92ccab83b184d4701b61f9

Pass HTTP_PROXY and HTTPS_PROXY to autologin

autologin_middleware/middleware.py
@@ -26,13 +26,15 @@ class AutologinMiddleware:
     def __init__(self, autologin_url, crawler):
         self.crawler = crawler
         s = crawler.settings
+        self.passed_setting = {
+            name: s.get(name) for name in [
+                'SPLASH_URL', 'USER_AGENT', 'HTTP_PROXY', 'HTTPS_PROXY']
+            if s.get('name')}
         self.autologin_url = autologin_url
-        self.splash_url = s.get('SPLASH_URL')
         self.login_url = s.get('AUTOLOGIN_LOGIN_URL')
         self.username = s.get('AUTOLOGIN_USERNAME')
         self.password = s.get('AUTOLOGIN_PASSWORD')
         self.extra_js = s.get('AUTOLOGIN_EXTRA_JS')
-        self.user_agent = s.get('USER_AGENT')
         self.autologin_download_delay = s.get('AUTOLOGIN_DOWNLOAD_DELAY')
         self.logout_url = s.get('AUTOLOGIN_LOGOUT_URL')
         # _force_skip and _n_pend and for testing only
@@ -129,7 +131,7 @@ class AutologinMiddleware:
         logger.debug('Attempting login at %s', request.url)
         autologin_endpoint = urljoin(self.autologin_url, '/login-cookies')
         params = {
-            'url': urljoin(request.url, self.login_url) \
+            'url': urljoin(request.url, self.login_url)
                    if self.login_url else request.url,
             'username': self.username,
             'password': self.password,
@@ -138,13 +140,9 @@ class AutologinMiddleware:
                 'ROBOTSTXT_OBEY': False,
             }
         }
-        if self.splash_url and 'splash' in request.meta:
-            params['settings']['SPLASH_URL'] = self.splash_url
-        if self.user_agent:
-            params['settings']['USER_AGENT'] = self.user_agent
+        params['settings'].update(self.passed_setting)
         if self.autologin_download_delay:
-            params['settings']['DOWNLOAD_DELAY'] = \
-                self.autologin_download_delay
+            params['settings']['DOWNLOAD_DELAY'] = self.autologin_download_delay
         return scrapy.Request(
             autologin_endpoint, method='POST',
             body=json.dumps(params).encode(),

commit da3dbcadd65c908a534595e30d65a8395d16e024

Do not pass SPLASH_URL if not using splash
Add a test and fix for regression introduced in
da9751bcc373087e2e92ccab83b184d4701b61f9.

autologin_middleware/middleware.py
@@ -141,6 +141,9 @@ class AutologinMiddleware:
             }
         }
         params['settings'].update(self.passed_setting)
+        if 'splash' not in request.meta:
+            # if request does not use splash, do not pass SPLASH_URL
+            params['settings'].pop('SPLASH_URL', None)
         if self.autologin_download_delay:
             params['settings']['DOWNLOAD_DELAY'] = self.autologin_download_delay
         return scrapy.Request(

tox.ini
@@ -12,5 +12,5 @@ deps=
 
 commands=
     pip install -U pip wheel
-    pip install scrapy==1.1.0
+    pip install scrapy==1.1.0 scrapy_splash
     py.test --doctest-modules --cov=autologin_middleware {posargs: autologin_middleware tests}

tests/test_integration.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import json
 import uuid
 from six.moves.urllib.parse import urlsplit, urlunsplit
 
@@ -9,13 +10,14 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.linkextractors import LinkExtractor
 from scrapy.utils.log import configure_logging
 from scrapy.utils.python import to_bytes
+from scrapy_splash import SplashRequest
 from twisted.internet import defer, reactor
 from twisted.trial.unittest import TestCase
 from twisted.web.resource import Resource
 from twisted.web.server import NOT_DONE_YET
 from twisted.web.util import Redirect
 
-from autologin_middleware import link_looks_like_logout
+from autologin_middleware import AutologinMiddleware, link_looks_like_logout
 from .mockserver import MockServer
 
 
@@ -289,3 +291,26 @@ class TestAutoLoginCustomHeaders(SpiderTestCase):
         spider = self.crawler.spider
         assert len(spider.visited_urls) == 2
         assert spider.visited_urls[1] == '/hidden'
+
+
+class TestAutologinRequest(SpiderTestCase):
+    settings = {
+        'SPLASH_URL': 'http://192.168.99.100:8050',
+    }
+    def test(self):
+        mw = AutologinMiddleware('http://127.0.0.1:8089', self.crawler)
+        al_request = mw._login_request(scrapy.Request('http://example.com'))
+        data = json.loads(al_request.body.decode('utf-8'))
+        assert al_request.dont_filter
+        assert al_request.meta['proxy'] is None
+        assert data['url'] == 'http://example.com'
+        assert data['settings']['USER_AGENT'] == \
+               self.crawler.settings.get('USER_AGENT')
+        assert data['settings'].get('SPLASH_URL') is None
+
+        al_request = mw._login_request(SplashRequest('http://example.com'))
+        data = json.loads(al_request.body.decode('utf-8'))
+        assert data['url'] == 'http://example.com'
+        assert data['settings']['SPLASH_URL'] == \
+               self.crawler.settings.get('SPLASH_URL')
+