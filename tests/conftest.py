daf1b5613804e6c0cb787c59a777aafdb911371a
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-05-24 18:39:28 +0300

    Extend settings to run splash tests, splash helper

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
+import os
+
 import pytest
-from scrapy.crawler import CrawlerRunner
 
 
 pytest_plugins = 'pytest_twisted'
@@ -11,16 +12,38 @@ def base_settings():
         'AUTOLOGIN_ENABLED': True,
         # Higher fixed value to make the test more reliable
         'AUTOLOGIN_MAX_LOGOUT_COUNT': 8,
-        'COOKIES_ENABLED': True,
-        'COOKIES_DEBUG': True,
         'DOWNLOADER_MIDDLEWARES': {
             'autologin_middleware.AutologinMiddleware': 605,
-            'scrapy.downloadermiddlewares.cookies.CookiesMiddleware': None,
-            'autologin_middleware.ExposeCookiesMiddleware': 700,
         }
     }
 
 
-@pytest.fixture(params=[False])
+@pytest.fixture(params=[False, True])  # use splash
 def settings(request):
-    return base_settings()
+    use_splash = request.param
+    s = base_settings()
+    if use_splash:
+        s.update({
+            'SPLASH_URL': os.environ.get('SPLASH_URL', 'http://127.0.0.1:8050'),
+            'SPLASH_COOKIES_DEBUG': True,
+            'SPIDER_MIDDLEWARES': {
+                'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+            },
+            'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
+        })
+        s['DOWNLOADER_MIDDLEWARES'].update({
+            'scrapy_splash.SplashCookiesMiddleware': 723,
+            'scrapy_splash.SplashMiddleware': 725,
+            'scrapy.downloadermiddlewares.httpcompression'
+            '.HttpCompressionMiddleware': 810,
+        })
+    else:
+        s.update({
+            'COOKIES_ENABLED': True,
+            'COOKIES_DEBUG': True,
+        })
+        s['DOWNLOADER_MIDDLEWARES'].update({
+            'scrapy.downloadermiddlewares.cookies.CookiesMiddleware': None,
+            'autologin_middleware.ExposeCookiesMiddleware': 700,
+        })
+    return s
